b5f61828d48d757d2261012703c9a48042299c07
Philipp Hanslovsky <philipp.hanslovsky@gmail.com>
2024-07-18 08:56:21 -0400

    First attempt to support different types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use ansi_term::Colour;
 use ansi_term::Colour::RGB;
 use bytes::Bytes;
 use clap::Parser;
@@ -6,19 +7,17 @@ use image::ImageBuffer;
 use image::ImageError;
 use image::{Rgb, Rgba};
 use image::Pixel;
-use image::Primitive;
 use image::io::Reader;
 use reqwest;
 use std::error::Error;
 use std::fmt;
 use std::io::Cursor;
 use std::ops::Deref;
+use std::option::Option;
 use std::str;
 use std::time::Duration;
 use std::time::SystemTime;
 use std::time::UNIX_EPOCH;
-
-trait TColRError: Error + fmt::Display {}
 
 #[derive(Debug)]
 enum ImageFromUriError {
@@ -128,7 +127,41 @@ impl RGBSum {
     }
 }
 
-pub trait Aggregator<P> {
+trait Div<T> {
+   fn div_inplace(&mut self, divisor: T);
+}
+
+trait IsSame {
+    fn is_same(&self, color: &Colour) -> bool;
+}
+
+impl Div<u64> for RGBSum {
+    fn div_inplace(&mut self, divisor: u64) {
+        self.div(divisor);
+    }
+}
+
+impl IsSame for RGBSum {
+    fn is_same(&self, color: &Colour) -> bool {
+        return match color {
+            // TODO can we avoid r.clone().into() here?
+            Colour::RGB(r, g, b ) => self.r == r.clone().into() && self.g == g.clone().into() && self.b == b.clone().into(),
+            _ => false
+        }
+    }
+}
+
+trait ToColour {
+    fn to_colour(&self) -> Colour;
+}
+
+impl ToColour for RGBSum {
+    fn to_colour(&self) -> Colour {
+        return Colour::RGB(self.r as u8, self.g as u8, self.b as u8);
+    }
+}
+
+trait Aggregator<P>: Div<u64> {
     fn aggregate(&mut self, p: &P);
 }
 
@@ -148,22 +181,6 @@ impl <U: Into<u64> + Copy> Aggregator<Rgba<U>> for RGBSum {
     }
 }
 
-// impl <U: Into<u64> + Copy> AddRGB<Rgb<U>> for RGBSum {
-//     fn add_rgb(&mut self, rgb: &Rgb<U>) {
-//         self.r += rgb[0].into();
-//         self.g += rgb[1].into();
-//         self.b += rgb[2].into();
-//     }
-// }
-
-// impl <U: Into<u64> + Copy> AddRGB<Rgba<U>> for RGBSum {
-//     fn add_rgb(&mut self, rgb: &Rgba<U>) {
-//         self.r += rgb[0].into();
-//         self.g += rgb[1].into();
-//         self.b += rgb[2].into();
-//     }
-// }
-
 impl Clone for RGBSum {
     fn clone(&self) -> Self {
         return RGBSum {
@@ -173,28 +190,6 @@ impl Clone for RGBSum {
         }
     }
 }
-
-
-// fn sum_chunks<P: Pixel, C: Deref<Target = [P::Subpixel]>>(
-//     image: &ImageBuffer<P, C>,
-//     chunk_width: u32,
-//     row: u32
-// ) -> Vec<RGBSum> {
-//     let n = image.width() / chunk_width;
-//     let mut rgbs: Vec<RGBSum> = Vec::new();
-//     for idx in 0 .. n {
-//         let start = idx * chunk_width;
-//         let stop = start + chunk_width;
-//         let mut rgb_sum = RGBSum::zero();
-//         for x in start .. stop {
-//             let rgb = image.get_pixel(x, row);
-//             rgb_sum.aggregate(&rgb);
-//         }
-//         rgbs.push(rgb_sum);
-//     }
-//     return rgbs;
-// }
-
 
 fn sum_chunks_inplace<P: Pixel, Agg: Aggregator<P>, C: Deref<Target = [P::Subpixel]>>(
     image: &ImageBuffer<P, C>,
@@ -246,18 +241,6 @@ impl RgbCount {
 }
 
 
-// struct MyUriArg {
-//     url: Uri<String>
-// }
-
-// impl str::FromStr for MyUriArg {
-    
-// }
-
-
-// let default_uri: Uri<String> = Uri::parse("/home/zottel/Pictures/atze.jpg").unwrap();
-// let default_url: Uri<String> = Uri::builder().path();
-
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -284,76 +267,64 @@ fn main() {
     let args = Args::parse();
     let t1 = get_time();
 
-    let image = match get_image(&args.image_url) {
-        Ok(i) => i,
-        Err(error) => panic!("Unable to open image for uri {}: {:?}", args.image_url, error)
-    };
-    
-    // let buf = image.into_rgb8();
-    let buf = match image.as_rgb8() {
-        Some(b) => b,
-        None => panic!("Not an RGB image")
-    };
-    let t2 = get_time();
-
     let chunks_x = args.x_chunks;
     let chunks_y = args.y_chunks;
 
-    let n_x = image.width() as usize / chunks_x;
-    let n_y = image.height() as usize / chunks_y;
+    let image = match get_image(&args.image_url) {
+        Ok(i) => match i {
+            DynamicImage::ImageRgb8(buf) => parse_image_and_print(&buf, chunks_x, chunks_y, RGBSum::zero()),
+            _ => panic!("Unsupported pixel type:")
+        }
+        Err(error) => panic!("Unable to open image for uri {}: {:?}", args.image_url, error)
+    };
+}
+
+fn parse_image_and_print<P: Pixel, Agg: Aggregator<P> + Clone + IsSame + ToColour, C: Deref<Target = [P::Subpixel]>>(
+    buf: &ImageBuffer<P, C>,
+    chunks_x: usize,
+    chunks_y: usize,
+    zero_agg: Agg
+) {
+    let n_x = buf.width() as usize / chunks_x;
+    let n_y = buf.height() as usize / chunks_y;
     let n = chunks_x as u64 * chunks_y as u64;
 
-    let mut rgbs = vec![RGBSum::zero(); n_x * n_y];
+    let mut rgbs = vec![zero_agg.clone(); n_x * n_y];
 
     for y_chunk in 0 .. n_y {
         let start = y_chunk * chunks_y;
         let stop = start + chunks_y;
-        // for x in 0 .. n_x as usize {
-        //     rgbs[x].set_zero();
-        // };
 
         let mut slice = &mut rgbs[y_chunk * n_x .. y_chunk * n_x + n_x];
         for y in start .. stop {
-            // let lrgbs = sum_chunks(buf, chunks_x as u32, y as u32);
-            // for x in 0 .. n_x {
-            //     rgbs[x].add(&lrgbs[x]);
-            // }
             sum_chunks_inplace(&buf, chunks_x as u32, y as u32, slice);
         }
         for rgb in slice {
-            rgb.div(n as u64);
+            rgb.div_inplace(n as u64);
         }
     }
-    let t3 = get_time();
-
-    // let mut print_str = String::new();
     
     for y_chunk in 0 .. n_y {
-        let mut rgb_count = RgbCount::invalid();
         let slice = &rgbs[y_chunk * n_x .. y_chunk * n_x + n_x];
-        // for y in start .. sntop {
-        // let rgb = &rgbs[x];
+        let mut prev: Colour = Colour::Black;
+        let mut counter: usize = 0;
         for rgb in slice {
-            if rgb_count.is_valid() {
-                if rgb_count.is_same_rgb(rgb) {
-                    rgb_count.incr();
-                } else {
-                    let c = RGB(rgb_count.rgb_sum.r as u8, rgb_count.rgb_sum.g as u8, rgb_count.rgb_sum.b as u8);
-                    print!("{}", c.paint("$".repeat(rgb_count.count)));
-                    // print_str = format!("{}{}", print_str, c.paint("$".repeat(rgb_count.count)));
-                    rgb_count.set_rgb(rgb.clone());
+            match counter {
+                0 => {
+                    prev = rgb.to_colour();
+                    counter += 1;
+                },
+                _ => {
+                    if rgb.is_same(&prev) {
+                        counter += 1;
+                    } else {
+                        print!("{}", prev.paint("$".repeat(counter)));
+                        counter = 0;
+                    }
                 }
-            } else {
-                rgb_count.set_rgb(rgb.clone());
             }
         }
-        let c = RGB(rgb_count.rgb_sum.r as u8, rgb_count.rgb_sum.g as u8, rgb_count.rgb_sum.b as u8);
-        print!("{}", c.paint("$".repeat(rgb_count.count)));
+        print!("{}", prev.paint("$".repeat(counter)));
         print!("\n");
-        // print_str = format!("{}{}\n", print_str, c.paint("$".repeat(rgb_count.count)));
     }
-    // print!("{}", print_str);
-    let t4 = get_time();
-
-    // println!("args: {:#?} img: {:#?} transform: {:#?} print: {:#?}", t1 - t0, t2 - t1, t3 - t2, t4 - t3);
 }
